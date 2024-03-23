47b0813b48b18ba7b7bcddc4a79b1b4e176ba4cc
WladiCosta <wladimircosta.dev@gmail.com>
2024-03-22 21:19:44 -0300

    add StarCanvas

--- components/sub/HeroContent.jsx
+++ components/sub/HeroContent.jsx
@@ -16,7 +16,7 @@ const HeroContent = () => {
         <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
             <motion.div
                 variants={slideInFromTop}
-                className='Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9]'>
+                className='Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]'>
                     <SparklesIcon className='text-[#b49bff] mr-10px h-5 w-5' />
                     <h1 className='Welcome-text text-[13px]'>
                         Front-End Dev Portfolio
@@ -27,9 +27,11 @@ const HeroContent = () => {
              variants={slideInFromLeft(0.5)}
              className='flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'
             >
-               Oferecendo a
-               <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'>melhor experiência</span>
-               de projeto
+              <span>
+                Oferecendo a
+                <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> melhor experiência </span>
+                de projeto
+              </span>
             </motion.div>
 
             <motion.p 
