f387378f70be3ceb465f5b33fea417f21c88d99a
WladiCosta <wladimircosta.dev@gmail.com>
2024-03-22 23:04:46 -0300

    add meta viewport

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,13 @@ import Hero from "@/components/main/Hero";
 
 export default function Home() {
   return (
+    <><>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </>
     <main className="h-full w-full">
-     <div className="flex flex-col gap-20">
-      <Hero />
-     </div>
-    </main>
+        <div className="flex flex-col gap-20">
+          <Hero />
+        </div>
+      </main></>
   );
 }
---
+++ components/sub/SkillDataProvider.jsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from 'react'
+import { motion } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+
+
+
+const SkillDataProvider = () => {
+  return (
+    <div>SkillDataProvider</div>
+  )
+}
+
+export default SkillDataProvider
